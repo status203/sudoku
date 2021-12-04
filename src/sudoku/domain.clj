d7a9c7cbde2f3b402ed4b5d3585ca3ff6962f210
status203 <git@status203.com>
2021-12-04 17:19:28 +0000

    A bunch of grid helper fns

--- src/sudoku/domain.clj
+++ src/sudoku/domain.clj
@@ -3,13 +3,72 @@
 (def full-set (set (take 9 (iterate inc 1))))
 
 (defn prepare-grid
-  "Takes a 1D vector of integers (set values) and nils (empty cells) 
+  "Takes a 1D vector of integers (initally set values) and nils (empty cells) 
    representing an initial sudoku grid and replaces all nils with a full set 
    representing possible entries."
   [grid]
   (map #(or % full-set) grid))
 
 
+(defn cell->row-index
+  "Takes a 1D cell location and returns the 0-based row index of that cell"
+  [cell]
+  (quot cell 9))
+
+(defn cell->col-index
+  "Takes a 1D cell location and returns the 0-based column index of that cell"
+  [cell]
+  (rem cell 9))
+
+(defn cell->subgrid-idxs
+  "Takes a 1D cell location and returns the 0-based indices of [row column] of
+   the cell"
+  [cell]
+  (let [row-idx (cell->row-index cell)
+        col-idx (cell->col-index cell)]
+    [(quot row-idx 3) (quot col-idx 3)]))
+
+(defn cell->row
+  "Takes a grid and a 1D cell location and returns the row containing that cell"
+  [grid cell]
+  (let [idx (cell->row-index cell)]
+    (take 9 (drop (* 9 idx) grid))))
+
+(flatten [[1 [2 3 [] [4] 5] 6 7]])
+
+(defn cell->col
+  "Takes a grid and a 1D cell location and returns the column containing that cell"
+  [grid cell]
+  (let [idx (cell->col-index cell)]
+    (->> (partition 9 grid)
+        (map #(first (drop idx %))))))
+
+(defn cell->subgrid
+  "Takes a grid and a 1D cell location and returns the subgrid containing that cell"
+  [grid cell]
+  (let [[sg-row-idx sg-col-idx] (cell->subgrid-idxs cell)]
+    (->> grid
+         (partition 9)
+         (drop (* 3 sg-row-idx))
+         (take 3)
+         (map #(take 3 (drop (* 3 sg-col-idx) %)))
+         flatten)))
+
 (comment
+  (def easy1
+    (let [_ nil]
+      [6 7 _   _ 3 8   4 2 9
+       3 1 4   _ 9 5   _ 8 6
+       _ _ 2   _ 4 7   _ _ _
+
+       _ _ 1   _ 6 _   _ _ _
+       _ 3 _   8 _ _   5 _ 4
+       _ _ _   3 2 _   _ 6 7
+
+       5 6 _   _ 8 _   _ _ _
+       _ _ _   4 _ _   _ 5 _
+       1 _ 8   _ _ _   6 _ 2]))
+  
+  (cell->subgrid easy1 80)
   )
 
